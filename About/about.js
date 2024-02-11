fea9df6116218eda93f8016ca4ddb1600e257983
Gabrielle Ann Franco <156222595+gabfranc@users.noreply.github.com>
2024-02-11 15:12:31 -0700

    Add files via upload

---
+++ About/about.js
@@ -0,0 +1,27 @@
+function showSidebar(){
+  const nav = document.querySelector('navBar')
+  nav.style.display= 'flex'
+}
+
+function hideSidebar(){
+  const nav = document.querySelector('navBar')
+  nav.style.display= 'none'
+}
+
+var navIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(navIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("nav_list");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
---
+++ Contact/contact.js
@@ -0,0 +1,28 @@
+function showSidebar(){
+  const nav = document.querySelector('nav')
+  nav.style.display= 'flex'
+}
+
+function hideSidebar(){
+  const nav = document.querySelector('nav')
+  nav.style.display= 'none'
+}
+
+var navIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(navIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("nav_list");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
